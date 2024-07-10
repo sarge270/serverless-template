fae9a4f9cbd59421d51139a6eaab481cfde40717
John <john.kim@dencomm.ai>
2024-07-10 13:19:27 +0900

    Add jest config

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type {Config} from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  testTimeout: 10000,
+};
+
+export default config;
---
+++ test/hello.spec.ts
@@ -0,0 +1,7 @@
+import {hello} from "../handler/hello";
+
+describe('hello handler test', () => {
+  it('should return a message', async () => {
+    expect(true).toEqual(true)
+  });
+});
